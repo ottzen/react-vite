767810e862003738fab1c8c614ddcc0dd36c6816
Kasper Ottzen <ko@picopublish.com>
2025-04-10 08:11:54 +0200

    Working spinner

--- src/components/templates/matador/matador.component.tsx
+++ src/components/templates/matador/matador.component.tsx
@@ -64,7 +64,7 @@ const MatadorComponent = () => {
                 )}
             </div>
 
-            {!shouldShowResults && showResults && (
+            {searchQuery !== "" && !showResults && (
                 <Spinner />
             )}
 
